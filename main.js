9907427c1fbef9338278a891778aff4a6f290647
kadunapikin <ibakoshi27@gmail.com>
2023-03-31 04:38:28 +0100

    write a create getSize function

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-createBoard(16);
+document.addEventListener("DOMContentLoaded", function() {
+    createBoard(16);
+
+})
+
 function createBoard(size) {
-    const mainContainer = document.querySelector(".main-container");
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
     let numDivs = size * size;
-    for (i = 0; i < numDivs; i++);
-    let div = document.createElement("div");
-    div.style.backgroundColor = "yellow";
-    mainContainer.appendChild(div);
+    
+    for (i = 0; i < numDivs; i++) {
+        let div = document.createElement('div');
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
+    }
 }
+    // div.addEventListener("mouseover", function() {
+    //     div.style.backgroundColor = "black";
+    // })
+    // // mainContainer.appendChild(div);
+
+
+function getSize() {
+    let input = prompt("What will be the size?");
+    let message = document.querySelector("#message");
+    if (input == "") {
+        message.innerHTML = "Please provide a number";
+    } else if (input < 0 || input > 100) {
+        message.innerHTML = "Please provide a number between 1-100"
+    } else {
+       message.innerHTML = "Now you can play!";
+       return input;
+    }
+}
+
